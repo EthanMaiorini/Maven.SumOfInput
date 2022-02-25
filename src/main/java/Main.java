b4b6ee5d96a48124d01d052eaa8f59caa4dafffc
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-02-24 22:03:51 -0500

    I made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Main main_object = new Main();
+        Scanner newscanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int newN = newscanner.nextInt();
+        int result = main_object.sumOfNumbers(newN);
+        System.out.println(result);
+        newscanner.close();
+    }
 
+    public int sumOfNumbers(int n){
+        int sum = 0;
+        for(int i = 1; i<=n;i++){
+            sum +=i;
+        }
+        return sum;
     }
 }
